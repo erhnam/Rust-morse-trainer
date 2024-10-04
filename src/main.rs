332fcca816ed1d60fca69ae8b0b842fa0db9f753
Paco <farcos@sirden.es>
2024-10-04 08:35:35 +0200

    Remove wiringx and add linux-embedded-hal

--- src/main.rs
+++ src/main.rs
@@ -21,68 +21,77 @@ The space between symbols (dots and dashes) of the same letter is 1 time unit.
 The space between letters is 3 time units.
 The space between words is 7 time units
 */
-use wiringx::{
-    gpio::{Input, Output, Value},
-    platform::Platform,
-    WiringX,
-};
 
+use linux_embedded_hal::sysfs_gpio::{Direction, Pin};
 use std::time::Instant;
 use std::{thread, time::Duration};
+use std::collections::HashMap;
 
 mod morse;
 
 const END_CODE: u128 = 2000;
-const PIN_LED: i32 = 25;
-const PIN_BUTTON: i32 = 22;
-const PIN_BUZZER: i32 = 15;
+const PIN_LED: u64 = 25;
+const PIN_BUTTON: u64 = 22;
+const PIN_BUZZER: u64 = 15;
 
-/*
-BCM  -  Wiring X
- 1   -     0
- 2   -     1
- 4   -     2
- 5   -     3
- 6   -     4
- 7   -     5
- 9   -     6
-10   -     7
-11   -     8
-12   -     9
-14   -     10
-15   -     11
-16   -     12
-17   -     13
-19   -     14
-20   -     15
-21   -     16
-22   -     17
-24   -     18
-25   -     19
-26   -     20
-27   -     21
-29   -     22
-30   -     23 RUN
-LED   -    25
-31   -     26
-32   -     27
-*/
+fn gpio_get_pin(pin_num: u64) -> u64 {
+    let pin_map: HashMap<u64, u64> = [
+        (1, 508),
+        (2, 509),
+        (4, 378),
+        (5, 377),
+        (6, 371),
+        (7, 372),
+        (9, 375),
+        (10, 374),
+        (11, 373),
+        (12, 370),
+        (14, 425),
+        (15, 426),
+        (16, 496),
+        (17, 497),
+        (19, 494),
+        (20, 495),
+        (21, 503),
+        (22, 504),
+        (24, 502),
+        (25, 505),
+        (26, 507),
+        (27, 506),
+        (29, 356),
+        (41, 440),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
+    *pin_map.get(&pin_num).unwrap_or(&0)
+}
 
 #[tracing::instrument]
 fn main() -> anyhow::Result<()> {
 
-    let wiringx = WiringX::new(Platform::MilkVDuo).unwrap();
-
     let subscriber = tracing_subscriber::FmtSubscriber::new();
+
     if tracing::subscriber::set_global_default(subscriber).is_err() {
         tracing::error!("Can't set global tracing::subscriber default");
     }
 
     tracing::info!("Morse Code");
 
-    let led = wiringx.gpio_pin::<Output>(PIN_LED).unwrap();
-    let button = wiringx.gpio_pin::<Input>(PIN_BUTTON).unwrap();
-    let buzzer = wiringx.gpio_pin::<Output>(PIN_BUZZER).unwrap();
+    let led = Pin::new(gpio_get_pin(PIN_LED));
+    led.export().expect("error exporting cs pin");
+    led.set_direction(Direction::Out).expect("error setting cs pin direction");
+    led.set_value(0).unwrap();
+
+    let buzzer = Pin::new(gpio_get_pin(PIN_BUZZER));
+    buzzer.export().expect("error exporting cs pin");
+    buzzer.set_direction(Direction::Out).expect("error setting cs pin direction");
+    buzzer.set_value(0).unwrap();
+
+    let button = Pin::new(gpio_get_pin(PIN_BUTTON));
+    button.export().expect("error exporting cs pin");
+    button.set_direction(Direction::In).expect("error setting cs pin direction");
 
     let mut time_button_pressed: Option<Instant> = None;
     let mut time_end_secuence: Option<Instant> = None; // Detect end transmission
@@ -92,10 +101,10 @@ fn main() -> anyhow::Result<()> {
     let mut total_presses: u128 = morse::TOTAL_PRESS; // Número total de pulsaciones
 
     loop {
-        if button.read() == Value::High {
+        if button.get_value().unwrap() == 1 {
             if time_button_pressed.is_none() {
                 time_button_pressed = Some(Instant::now());
-                buzzer.write(Value::High);
+                buzzer.set_value(1).unwrap();
             }
 
             if let Some(released) = time_button_released {
@@ -108,11 +117,11 @@ fn main() -> anyhow::Result<()> {
                 time_button_released = None;
             }
 
-            led.write(Value::High);
+            led.set_value(1).unwrap();
         } else {
             if time_button_released.is_none() {
                 time_button_released = Some(Instant::now());
-                buzzer.write(Value::Low);
+                buzzer.set_value(0).unwrap();
             }
             if let Some(pressed) = time_button_pressed {
                 let elapsed_time = Instant::now().duration_since(pressed);
@@ -128,7 +137,7 @@ fn main() -> anyhow::Result<()> {
                 time_button_pressed = None;
                 time_end_secuence = Some(Instant::now());
             }
-            led.write(Value::Low);
+            led.set_value(0).unwrap();
         }
 
         // Check if end transmission
